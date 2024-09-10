8f2a5c52e75adb978620d9aacbbd122ba452e9ab
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-09-10 19:41:32 +0200

    spi master finished but not teste

--- src/SPI/src/spi_master.sv
+++ src/SPI/src/spi_master.sv
@@ -15,18 +15,21 @@ module spi_master
 
     // MOSI Signals
     input       [FRAMEWIDTH-1:0] i_TX_Byte,   // Send data
-    input       i_TX_dv,                      // TX Data valid
-    output reg  r_TX_Ready,
+    input       i_TX_DV,                      // TX Data valid
+    output reg  o_TX_Ready,
 
     // MISO Signals
-    output reg  r_RX_dv,                      // RX Data valid
-    output reg  [FRAMEWIDTH-1:0] r_RX_Byte,   // Receive data
+    output reg  o_RX_DV,                      // RX Data valid
+    output reg  [FRAMEWIDTH-1:0] o_RX_Byte,   // Receive data
 
     // SPI Interface
-    output reg  r_SPI_Clk,
+    output reg  o_SPI_Clk,
     input       i_SPI_MISO,
-    output reg  r_SPI_MOSI
+    output reg  o_SPI_MOSI
     );
+
+    // Params
+    localparam unsigned CounterMaxVal = $clog2(FRAMEWIDTH);
 
     // SPI Interface ~~ SPI Clock Domain
     wire w_CPOL;        // Clock polarity
@@ -37,7 +40,7 @@ module spi_master
     reg [4:0] r_SPI_Clk_Edges;
     reg r_Leading_Edge;
     reg r_Trailing_Edge;
-    reg r_TX_dv;
+    reg r_TX_DV;
     reg [FRAMEWIDTH-1:0] r_TX_Byte;
 
     reg [$clog2(FRAMEWIDTH)-1:0] r_RX_Bit_Count;
@@ -51,45 +54,104 @@ module spi_master
     //             in side captures data on leading edge of clock
     // CPOL = 1 -> out side changes the data on leading edge of clock
     //             in side captures the data on the trailing edge of clock
-    assign w_CHPA = (SPI_MODE == 1) | (SPI_MODE == 3);
+    assign w_CPHA = (SPI_MODE == 1) | (SPI_MODE == 3);
 
     always @(posedge clk or negedge rstn) begin
         if (~rstn) begin
-            o_TX_Ready      <= 1'b0;
+            o_TX_Ready      <= 0;
             r_SPI_Clk_Edges <= 0;
-            r_Leading_Edge  <= 1'b0;
-            r_Trailing_Edge <= 1'b0;
+            r_Leading_Edge  <= 0;
+            r_Trailing_Edge <= 0;
             r_SPI_Clk       <= w_CPOL;
             r_SPI_Clk_Count <= 0;
         end else begin
-            r_Leading_Edge  <= 1'b0;
-            r_Trailing_Edge <= 1'b0;
+            r_Leading_Edge  <= 0;
+            r_Trailing_Edge <= 0;
 
-            if (i_TX_dv) begin
-                o_TX_Ready      <= 1'b0;
+            if (i_TX_DV) begin
+                o_TX_Ready      <= 0;
                 r_SPI_Clk_Edges <= 16;
             end else if (r_SPI_Clk_Edges > 0) begin
-                o_TX_Ready <= 1'b0;
+                o_TX_Ready <= 0;
 
                 if (r_SPI_Clk_Count == CLKS_PER_HALF_BIT*2 - 1) begin
-                    r_SPI_Clk_Edges <= r_SPI_Clk_Edges - 1'b1;
-                    r_Trailing_Edge <= 1'b1;
+                    r_SPI_Clk_Edges <= r_SPI_Clk_Edges - 1;
+                    r_Trailing_Edge <= 1;
                     r_SPI_Clk_Count <= 0;
                     r_SPI_Clk       <= ~r_SPI_Clk;
                 end else if (r_SPI_Clk_Count == CLKS_PER_HALF_BIT-1) begin
-                    r_SPI_Clk_Edges <= r_SPI_Clk_Edges - 1'b1;
-                    r_Leading_edge  <= 1'b1;
-                    r_SPI_Clk_Count <= r_SPI_Clk_Count + 1'b1;
+                    r_SPI_Clk_Edges <= r_SPI_Clk_Edges - 1;
+                    r_Leading_Edge  <= 1;
+                    r_SPI_Clk_Count <= r_SPI_Clk_Count + 1;
                     r_SPI_Clk       <= ~r_SPI_Clk;
                 end else begin
-                    r_SPI_Clk_Count <= r_SPI_Clk_Count + 1'b1;
+                    r_SPI_Clk_Count <= r_SPI_Clk_Count + 1;
                 end
             end else begin
-                o_TX_Ready <= 1'b1;
+                o_TX_Ready <= 1;
+            end
+        end
+    end
+
+    // Register i_TX_Byte when Data Valid is pulsed
+    always @(posedge clk or negedge rstn) begin
+        if (~rstn) begin
+            r_TX_Byte <= 0;
+            r_TX_DV   <= 0;
+        end else begin
+            r_TX_DV <= i_TX_DV;
+            if (i_TX_DV) begin
+                r_TX_Byte <= i_TX_Byte;
             end
         end
     end
 
     // Generate MOSI data
+    always @(posedge clk or negedge rstn) begin
+        if (~rstn) begin
+            o_SPI_MOSI     <= 0;
+            r_TX_Bit_Count <= CounterMaxVal;   // Send MSB first
+        end else begin
+            if (o_TX_Ready) begin
+                r_TX_Bit_Count <= CounterMaxVal;
+            end else if (r_TX_DV & ~w_CPHA) begin
+                o_SPI_MOSI     <= r_TX_Byte[CounterMaxVal];
+                r_TX_Bit_Count <= CounterMaxVal - 1;
+            end else if ((r_Leading_Edge & w_CPHA) | (r_Trailing_Edge & ~w_CPHA)) begin
+                r_TX_Bit_Count <= r_TX_Bit_Count - 1;
+                o_SPI_MOSI     <= r_TX_Byte[r_TX_Bit_Count];
+            end
+        end
+    end
 
+    // Read MISO data
+    always @(posedge clk or negedge rstn) begin
+        if (~rstn) begin
+            o_RX_Byte      <= 0;
+            o_RX_DV        <= 0;
+            r_RX_Bit_Count <= CounterMaxVal;
+        end else begin
+            o_RX_DV <= 0;
+
+            if (o_TX_Ready) begin
+                r_RX_Bit_Count <= CounterMaxVal;
+            end else if ((r_Leading_Edge & ~w_CPHA) | (r_Trailing_Edge & w_CPHA)) begin
+                o_RX_Byte[r_RX_Bit_Count] <= i_SPI_MISO;  // Sample data
+                r_RX_Bit_Count            <= r_RX_Bit_Count - 1;
+
+                if (r_RX_Bit_Count == 0) begin
+                    o_RX_DV <= 1;
+                end
+            end
+        end
+    end
+
+    // Add clock delay to signals for alignment
+    always @(posedge clk or negedge rstn) begin
+        if (~rstn) begin
+            o_SPI_Clk <= w_CPOL;
+        end else begin
+            o_SPI_Clk <= r_SPI_Clk;
+        end
+    end
 endmodule
